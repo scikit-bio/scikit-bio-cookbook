8fa48bda5896195463a89dcc091b9a6df2aa6ed6
Greg Caporaso <gregcaporaso@gmail.com>
2014-11-18 09:10:12 -0700

    MAINT: removed title cells from notebooks as titles are now in the header section

--- skbiocookbook/__init__.py
+++ skbiocookbook/__init__.py
@@ -15,13 +15,13 @@ from os.path import splitext
 __version__ = "0.0.0-dev"
 
 def cookbook_toc():
-    """Generate HTML string of the scikit-bio Cookbook Table of Contents
+    """Generate HTML string of the cookbook Table of Contents
     """
     toc = ["<h2>Table of Contents</h2><p>"]
     nb_paths = [fp for fp in glob('*.ipynb') if fp != "Index.ipynb"]
     for nb_path in nb_paths:
-        title = ' '.join(splitext(nb_path)[0].split('-'))
+        title = splitext(nb_path)[0]
         toc.append("<li><a href='%s'  target='_blank'>%s</a></li>" % \
-            (nb_path, title.title()))
+            (nb_path, title))
 
     return '\n'.join(toc)
